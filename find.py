9506006e061ed4a2bc68aeb302e0b2a78d12fb12
itsnhduc <nhduc.lhp@gmail.com>
2017-09-07 18:24:50 +0800

    Complete basic pkg find

--- find.py
+++ find.py
@@ -3,4 +3,21 @@ import json
 
 import requests
 
+if len(sys.argv) == 1:
+  print('Filter missing.')
+  quit()
 query = sys.argv[1]
+if len(query) == 0:
+  print('Filter missing.')
+  quit()
+
+path = 'http://localhost:3000/api/spmpackages?filter[where][name][regexp]=' + query
+res = requests.get(path)
+listing = res.json()
+
+print(str(len(listing)) + ' package(s) found.')
+for pkgInfo in listing:
+  pkgShortInfo = '|  ' + pkgInfo['name'] + '@' + pkgInfo['version']
+  print(pkgShortInfo)
+
+# q: find with author
