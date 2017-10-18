a5dd4ac66bae8ce711471c9f8c66aa9d49b11988
itsnhduc <nhduc.lhp@gmail.com>
2017-10-18 18:13:50 +0800

    Add facade validation & list module & launcher for linux

---
+++ list.py
@@ -0,0 +1,7 @@
+import json
+
+def main(args):
+  with open('spm.json', 'r+') as spmFile:
+    spmInfo = json.load(spmFile)
+    for pkgName, pkgVersion in spmInfo['dep'].items():
+      print('| * {}@{}'.format(pkgName, pkgVersion))
